c1a834d58d0b09552694bfcd07b601954d57f0d7
Ammar74 <ammaryasser3016@gmail.com>
2023-10-26 17:48:05 +0300

    add (addNote) logic in the bottomSheet

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:notes/views/simple_bloc_observer.dart';
 void main() async {
   Bloc.observer =SimpleBlocObserver();
   await Hive.initFlutter();
-  await Hive.openBox(kNotesBox);
+  await Hive.openBox<NoteModel>(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -1,5 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/add_note_cubit.dart';
+import 'package:notes/models/note_model.dart';
 import 'package:notes/widgets/custom_button.dart';
 import 'package:notes/widgets/custom_text_field.dart';
 
@@ -45,6 +48,12 @@ class _AddNoteFormState extends State<AddNoteForm> {
               onTap: (){
                 if(formKey.currentState!.validate()){
                   formKey.currentState!.save();
+                  var noteModel= NoteModel(
+                      title: title!,
+                      subTitle: subTitle!,
+                      date: DateTime.now().toString(),
+                      color: Colors.blueAccent.value);
+                  BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
                 }else{
                   autovalidateMode = AutovalidateMode.always;
                   setState(() {
