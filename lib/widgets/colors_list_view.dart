394d59347f557a087d6427405fa00832f25ac864
Ammar74 <ammaryasser3016@gmail.com>
2023-10-29 01:48:28 +0200

    add note color when creating the note

--- lib/widgets/colors_list_view.dart
+++ lib/widgets/colors_list_view.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/cubits/add_note_cubit.dart';
 
 class ColorItem extends StatelessWidget {
   const ColorItem({super.key, required this.isActive, required this.color});
@@ -57,6 +59,7 @@ class _ColorsListViewState extends State<ColorsListView> {
               child: GestureDetector(
                 onTap: (){
                   currentIndex=index;
+                  BlocProvider.of<AddNoteCubit>(context).color= colors[index];
                   setState(() {});
                 },
                 child: ColorItem(
